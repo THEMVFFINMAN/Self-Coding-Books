31fa8402d8b1e6f856857e480ccc71d21207d7a8
JJ <joshuajordanlowe@gmail.com>
2016-09-22 23:55:13 -0400

    Added client_handler
    Tested it, was able to get a "shell" so to speak so that was pretty cool

--- BlackHatPython/HomeMadeNetCat.py
+++ BlackHatPython/HomeMadeNetCat.py
@@ -71,6 +71,7 @@ def client_sender(buffer):
 
 def server_loop():
     global target
+    global port
     
     # if no target is defined, we listen on all interfaces
     if not len(target):
@@ -79,6 +80,8 @@ def server_loop():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((target,port))
     server.listen(5)
+
+    print "[+] Successfully started port on Port: {}".format(port)
     
     while True:
         client_socket, addr = server.accept()
@@ -99,10 +102,69 @@ def run_command(command):
         output = subprocess.check_output(command,stderr=subprocess.
         STDOUT, shell=True)
     except:
-        output = "Failed to execute command.\r\n"
+        output = "[-] Failed to execute command.\r\n"
         
     # send the output back to the client
     return output
+
+
+def client_handler(client_socket):
+    global upload
+    global execute
+    global command
+    
+    # check for upload
+    if len(upload_destination):
+        
+        # read in all of the bytes and write to our destination
+        file_buffer = ""
+        
+        # keep reading data until none is available
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            else:
+                file_buffer += data
+            
+            # now we take these bytes and try to write them out
+        try:
+            file_descriptor = open(upload_destination,"wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+            
+            # acknowledge that we wrote the file out
+            client_socket.send("[+] Successfully saved file to {}\r\n".format(upload_destination))
+        except:
+            client_socket.send("[-] Failed to save file to {}\r\n".format(upload_destination))
+                               
+    # check for command execution
+    if len(execute):
+                               
+        # run the command
+        output = run_command(execute)
+                               
+        client_socket.send(output)
+                               
+    # now we go into another loop if a command shell was requested
+    if command:
+                               
+        while True:
+            # show a simple prompt
+            client_socket.send("<BHP:#> ")
+                               
+            # now we receive until we see a linefeed
+            cmd_buffer = ""
+
+            # For handwritten Python clients, remember to include the \n
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+                               
+            # send back the command output
+            response = run_command(cmd_buffer)
+                               
+            # send back the response
+            client_socket.send(response)
 
 
 def main():
@@ -142,20 +204,20 @@ def main():
         else:
             assert False,"Unhandled Option"
 
-        # are we going to listen or just send data from stdin?
-        if not listen and len(target) and port > 0:
-            # read in the buffer from the commandline
-            # this will block, so send CTRL-D if not sending input
-            # to stdin
-            buffer = sys.stdin.read()
-            # send data off
-            client_sender(buffer)
-            
-        # we are going to listen and potentially
-        # upload things, execute commands, and drop a shell back
-        # depending on our command line options above
-        if listen:
-            server_loop()
+    # are we going to listen or just send data from stdin?
+    if not listen and len(target) and port > 0:
+        # read in the buffer from the commandline
+        # this will block, so send CTRL-D if not sending input
+        # to stdin
+        buffer = sys.stdin.read()
+        # send data off
+        client_sender(buffer)
+        
+    # we are going to listen and potentially
+    # upload things, execute commands, and drop a shell back
+    # depending on our command line options above
+    if listen:
+        server_loop()
 
 if __name__ == "__main__":
     main()
